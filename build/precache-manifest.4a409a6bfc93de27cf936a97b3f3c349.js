c8447485b20abc54bd8f3ff74419675f44c63810
alt-source <tahirabdelghafur@gmail.com>
2020-03-27 16:57:12 +0100

    robots Files

---
+++ build/precache-manifest.4a409a6bfc93de27cf936a97b3f3c349.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d62b26eefde9003b3ad1bafa07b88802",
+    "url": "/index.html"
+  },
+  {
+    "revision": "adf50a1e66eaa0ab2c82",
+    "url": "/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "abb5c91c572762ad33e6",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "adf50a1e66eaa0ab2c82",
+    "url": "/static/js/2.fff352fc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.fff352fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "abb5c91c572762ad33e6",
+    "url": "/static/js/main.4903a1bb.chunk.js"
+  },
+  {
+    "revision": "d90f59e775f29b1343aa",
+    "url": "/static/js/runtime-main.9522bcc9.js"
+  }
+]);
